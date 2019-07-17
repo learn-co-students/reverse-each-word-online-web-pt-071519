90662badcaa1ec79d399382daee739dfb02eefbd
salubrious-pipe-1423 <digregoriothomas@yahoo.com>
2019-07-17 04:06:30 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,18 @@
+# FIRST METHOD THAT PASSED
+def reverse_each_word(array)
+  new_array = array.split(" ")
+  reversed_array = new_array.each {|x| x.reverse!}
+  return reversed_array.join(" ")
+end
+
+# FIRST REFACTORED CODE
+def reverse_each_word(array)
+  new_array = array.split(" ")
+  new_array.collect {|x| x.reverse!}
+  new_array.join(" ")
+end
+
+# FINAL SOLUTION
+def reverse_each_word(array)
+  array.split(" ").collect {|x| x.reverse!}.join(" ")
+end
