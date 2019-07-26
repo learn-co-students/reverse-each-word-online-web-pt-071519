d81883942d84b6e86a13833eaa11547b92fb0c09
assane savadogo <assane.savadogo81@gmail.com>
2019-07-26 15:04:29 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,24 +1,18 @@
-def reverse_each_word(string)
+def reverse_each_word(phrase)
   new_arr = []
-  string = string.split(" ")
-  string.each do |word| 
+  phrase = phrase.split(" ")
+  phrase.each do |word| 
     new_arr << word.reverse 
   end
   new_arr.join(" ")
 end
-  
-def reverse_each_word(string)
+
+
+def reverse_each_word(phrase)
   new_arr = []
-  string = string.split(" ")
-  string.collect do |word|
-    new_arr.push(word.reverse)
-  end
+  phrase = phrase.split(" ")
+  phrase.collect do |word| 
+    new_arr << word.reverse 
+  end 
   new_arr.join(" ")
 end
-  
-  
-  
-  
-
-
-
