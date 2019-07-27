027e1ee5651f24cb72e205e1004dde6d06c0da26
Paul Beckwith <303dog@gmail.com>
2019-07-27 22:55:38 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,14 @@
+def reverse_each_word("Hello there, and how are you?")
+  ().each do |word|
+  end
+end
+
+def reverse_each_word("Hello there, and how are you?")
+  ().collect do |word|
+  end
+end    
+
+def reverse_each_word("Hello there, and how are you?")
+  ().map do |word|
+  end
+end
